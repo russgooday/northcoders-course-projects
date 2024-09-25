d340c7b9dd6eddfa6b3ea326d165e01c313430e8
russgooday <rpg_digital@yahoo.co.uk>
2024-09-25 15:45:40 +0100

    examples move to their own file

--- countem/src/find_most_repeated.py
+++ countem/src/find_most_repeated.py
@@ -1,5 +1,6 @@
 """
-Function to find the most repeated elements in a list and the number of times they repeat
+Utilising a custom Countem class the find_most_repeated function finds the 
+most repeated elements in a list and the number of times they repeat
 """
 
 from typing import List, Dict
@@ -26,17 +27,3 @@ def find_most_repeated(elems: List) -> Dict:
         return { 'elements': [], 'repeats': None }
 
     return { 'elements': list(counts.multi_mode()), 'repeats': maxcount }
-
-if __name__ == '__main__':
-    # Examples
-    print(find_most_repeated([]))
-    # {'elements': [], 'repeats': None}
-
-    print(find_most_repeated(['a','b','c']))
-    # {'elements': [], 'repeats': None}
-
-    print(find_most_repeated(['a','b','b','c']))
-    # {'elements': ['b'], 'repeats': 2}
-
-    print(find_most_repeated(['a', 'b', 'b', 3, 'c', 3, 'c', 3, 'b']))
-    # {'elements': ['b', 3], 'repeats': 3}
