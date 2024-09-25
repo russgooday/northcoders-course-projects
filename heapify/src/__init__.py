614c9fed991450bba6efe1c7f07a6490fcdd6e40
russgooday <rpg_digital@yahoo.co.uk>
2024-09-25 15:45:21 +0100

    package init files added

---
+++ heapify/src/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=missing-module-docstring
+from .internals._heapify import heapify, heap_replace
---
+++ heapify/src/internals/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=missing-module-docstring
+from . _heapify import heapify, heap_replace
