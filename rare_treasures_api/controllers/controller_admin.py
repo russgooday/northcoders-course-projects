38772e4dc06e33701952ecd6bd0ec98ff80d1ef2
russgooday <rpg_digital@yahoo.co.uk>
2024-10-17 00:56:24 +0100

    rename router endpoint

--- rare_treasures_api/controllers/controller_admin.py
+++ rare_treasures_api/controllers/controller_admin.py
@@ -8,7 +8,7 @@ router = APIRouter()
 templates = Jinja2Templates(directory=f'{root_dir}/views')
 
 # Add treasure item to a shop
-@router.get('/add-to-treasures', response_class=HTMLResponse)
+@router.get('/add-treasure', response_class=HTMLResponse)
 def add_to_treasure_page(request: Request) -> HTMLResponse:
     ''' add treasure item to treasures '''
     return templates.TemplateResponse(request, name='add-treasure.html')
