a678253cec6a6ace851ea951415ab042b5027467
russgooday <rpg_digital@yahoo.co.uk>
2024-10-17 13:56:39 +0100

    small fix to dotenv argument

--- rare_treasures_api/db/connection.py
+++ rare_treasures_api/db/connection.py
@@ -6,7 +6,7 @@ from fastapi import HTTPException
 from ..dependencies import root_dir
 
 APP_ENV = env.get('APP_ENV', default='dev')
-load_dotenv(dotenv_path=f'{root_dir}/.env.{APP_ENV}')
+load_dotenv(f'{root_dir}/.env.{APP_ENV}')
 
 print(f'\nAPP_ENV: {APP_ENV}')
 
