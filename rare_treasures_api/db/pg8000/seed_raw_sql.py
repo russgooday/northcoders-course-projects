676ae4eb6f663b4e7ad55f6a28ac60b3060e342d
russgooday <rpg_digital@yahoo.co.uk>
2024-10-25 02:14:22 +0100

    move to pg8000 sub-folder

---
+++ rare_treasures_api/db/pg8000/seed_raw_sql.py
@@ -0,0 +1,61 @@
+''' Seeds the database using the standard PG8000.native api. '''
+import json
+from .connection import CreateConnection
+from ...dependencies import ROOT_PATH
+from .tables_sql import (
+    DROP_TABLES,
+    SHOPS_SCHEMA,
+    TREASURES_SCHEMA,
+    POPULATE_SHOPS,
+    POPULATE_TREASURES
+)
+
+def seed_db(env='test'):
+    print('\U0001FAB4', 'Seeding Database...')
+
+    with CreateConnection() as db:
+        shops_path = f'{ROOT_PATH}/data/{env}-data/shops.json'
+        treasures_path = f'{ROOT_PATH}/data/{env}-data/treasures.json'
+
+        db.run(DROP_TABLES)
+        db.run(SHOPS_SCHEMA)
+        db.run(TREASURES_SCHEMA)
+
+        with open(shops_path, 'r', encoding='utf-8') as file:
+            rows = json.load(file)['shops']
+            row_count = 0
+
+            for row in rows:
+                db.run(
+                    POPULATE_SHOPS,
+                    shop_name=row['shop_name'],
+                    owner=row['owner'],
+                    slogan=row['slogan']
+                )
+                row_count += 1
+
+            print(f'Successfully seeded {row_count} rows to `shops` table in the database.')
+
+
+        if not (shops := db.run('SELECT * FROM shops')):
+            print('Nothing to seed! Please check the `shops` table.')
+            return
+
+        shop_ids = {shop[1]: shop[0] for shop in shops}
+
+        with open(treasures_path, 'r', encoding='utf-8') as file:
+            rows = json.load(file)['treasures']
+            row_count = 0
+
+            for row in rows:
+                db.run(
+                    POPULATE_TREASURES,
+                    treasure_name=row.get('treasure_name'),
+                    colour=row.get('colour'),
+                    age=row.get('age'),
+                    cost_at_auction=row.get('cost_at_auction'),
+                    shop_id=shop_ids.get(row['shop'])
+                )
+                row_count += 1
+
+            print(f'Seeded {row_count} rows to `treasures` table in the database. \U0001F44D')
